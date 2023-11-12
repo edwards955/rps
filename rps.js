5bccaab83c378492224aec3699f43d9f49499314
edwards955 <getignorantfriday@gmail.com>
2023-11-11 21:11:07 -0500

    Implement playRound()

--- rps.js
+++ rps.js
@@ -8,3 +8,35 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    switch(playerSelection.toLowerCase()) {
+        case 'rock':
+            if (computerSelection === 'Rock') {
+                return "It's a tie!";
+            } else if (computerSelection === 'Paper') {
+                return "You lose! Paper beats Rock";
+            } else {
+                return "You Win! Rock beats Scissors!";
+            }
+            break;
+        case 'paper':
+            if (computerSelection === "Paper") {
+                return "It's a tie!";
+            } else if (computerSelection === "Scissors") {
+                return "You Lose! Scissors beats Paper";
+            } else {
+                return "You Win! Paper beats Rock!";
+            }
+            break;
+        case 'scissors':
+            if (computerSelection === "Scissors") {
+                return "It's a tie!";
+            } else if (computerSelection === "Rock") {
+                return "You Lose! Rock beats Scissors!";
+            } else {
+                return "You Win! Scissors beats Paper!";
+            }
+            break;
+    }
+}
